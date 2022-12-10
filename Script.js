a5f09a25ac80071b8492ad5e35cd82dc81a7d0d4
JujuLaBest35 <120265274+JujuLaBest35@users.noreply.github.com>
2022-12-10 16:17:30 +0100

    initialiser dossier

---
+++ Script.js
@@ -0,0 +1,98 @@
+/*Variables pour tous les personnages reliées à leurs identité dans HTML*/
+var Perso_arbre=document.getElementById("id_arbre")
+var Monstre_tornade=document.getElementById("id_tornade")
+var Perso_plante=document.getElementById("id_plante")
+var Monstre_caillou=document.getElementById("id_caillou")
+var Perso_souche=document.getElementById("id_souche")
+var Monstre_feu=document.getElementById("id_feu")
+var Perso_riviere=document.getElementById("id_riviere")
+var attaque_arbre=document.getElementById("attaque_arbre")
+
+/*Variables PV pour les personnages (héros)*/
+var PV_Arbre=document.getElementById("PVArbre")
+var PV_Plante=document.getElementById("PVPLante")
+var PV_Souche=document.getElementById("PVPlante")
+var PV_Riviere=document.getElementById["PvRiviere"]
+
+/*Fonction attaquer*/
+function attaquer(){
+    Monstre_caillou.onclick = degats_caillou;
+    Monstre_feu.onclick = degats_feu;
+    Monstre_tornade.onclick = degats_tornade;
+    }
+
+/*Fonctions dégats pour l'action attaque pour tous les monstres qui enlève 10PV*/
+var pvTornadeHtml = document.getElementById("PVTornade");
+var pvTornade = "100";
+
+function degats_tornade(){
+    pvTornade -= 10;
+    pvTornadeHtml.innerHTML = pvTornade;
+}
+
+
+var pvCaillouHtml = document.getElementById("PVCaillou");
+var pvCaillou = "100";
+
+function degats_caillou(){
+    pvCaillou -= 10;
+    pvCaillouHtml.innerHTML = pvCaillou;
+}
+ 
+
+var pvFeuHtml = document.getElementById("PVFeu");
+var pvFeu = "100";
+
+function degats_feu(){
+    pvFeu -= 10;
+    pvFeuHtml.innerHTML = pvFeu;
+}
+
+
+/*Fonction pour l'attaque spéciale vague du personnage rivière*/
+function vague(){
+    Monstre_caillou.onclick = degats_caillou_vague;
+    Monstre_feu.onclick = degats_feu_vague;
+    Monstre_tornade.onclick = degats_tornade_vague;
+    }
+
+
+var pvTornadeHtml = document.getElementById("PVTornade");
+var pvTornade = "100";
+
+function degats_tornade_vague(){
+    pvTornade -= 20;
+    pvTornadeHtml.innerHTML = pvTornade;
+}
+
+var pvCaillouHtml = document.getElementById("PVCaillou");
+var pvCaillou = "100";
+
+function degats_caillou_vague(){
+    pvCaillou -= 20;
+    pvCaillouHtml.innerHTML = pvCaillou;
+}
+
+var pvFeuHtml = document.getElementById("PVFeu");
+var pvFeu = "100";
+
+function degats_feu_vague(){
+    pvFeu -= 20;
+    pvFeuHtml.innerHTML = pvFeu;
+}   
+
+/*Fonction soigner pour l'attaque spéciale du perso arbre*/
+function soigner(){
+    Perso_arbre.onclick = ;
+    Perso_plante.onclick = ;
+    Perso_souche.onclick = ;
+    Perso_riviere.onclick = 
+}
+
+var pvArbreHtml = document.getElementById("PVArbre");
+var pvArbre = "100";
+
+function soigner(){
+    pvArbre -= (-20);
+    pvArbreHtml.innerHTML = pvArbre;
+}
