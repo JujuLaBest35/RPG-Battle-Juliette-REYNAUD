5be7c733caea4bcc7e77bf605613c4e72910d32e
AlanOixDeCoco <120265274+JujuLaBest35@users.noreply.github.com>
2022-12-10 17:49:02 +0100

    tours et mort ennemis

--- Script.js
+++ Script.js
@@ -1,31 +1,80 @@
 /*Variables pour tous les personnages reliées à leurs identité dans HTML*/
 var Perso_arbre=document.getElementById("id_arbre")
-var Monstre_1=document.getElementById("id_tornade")
+var Monstre_caillou=document.getElementById("id_caillou")
 var Perso_plante=document.getElementById("id_plante")
-var Monstre_2=document.getElementById("id_caillou")
+var Monstre_feu=document.getElementById("id_feu")
 var Perso_souche=document.getElementById("id_souche")
-var Monstre_3=document.getElementById("id_feu")
+var Monstre_tornade=document.getElementById("id_tornade")
 var Perso_riviere=document.getElementById("id_riviere")
 var attaque_arbre=document.getElementById("attaque_arbre")
 
 /*Variables PV pour les personnages (héros)*/
 var PV_Arbre=document.getElementById("PVArbre")
-var PV_Plante=document.getElementById("PVPLante")
-var PV_Souche=document.getElementById("PVPlante")
-var PV_Riviere=document.getElementById["PvRiviere"]
+var PV_Plante=document.getElementById("PVPlante")
+var PV_Souche=document.getElementById("PVSouche")
+var PV_Riviere=document.getElementById("PVRiviere")
+
+var pvArbre = 100;
+var pvPlante = 100;
+var pvSouche = 100;
+var pvRiviere = 100;
 
 var tourPerso = 0;
+var persoVise = false;
+var tourDesPersos = true;
 
 function RandomInt(min, max){
     return (Math.floor(Math.random() * (max-min)) + min);
 }
 
+Monstre_caillou.onclick = degats_caillou;
+Monstre_feu.onclick = degats_feu;
+Monstre_tornade.onclick = degats_tornade;
+
 /*Fonction attaquer*/
-function attaquer(){
-    tourPerso = 0;
-    Monstre_caillou.onclick = degats_caillou;
-    Monstre_feu.onclick = degats_feu;
-    Monstre_tornade.onclick = degats_tornade;
+function attaquer(numeroPerso){
+    if(tourDesPersos){
+        if(numeroPerso == tourPerso){
+            persoVise = true;
+        }
+    }
+}
+
+function ennemisAttaquent(){
+    for(ennemi = 0; ennemi < 3; ennemi++){
+        cible = RandomInt(0, 5);
+        switch(cible){
+            case 0:
+                pvArbre -= 10;
+                PV_Arbre.innerHTML = pvArbre;
+                if(pvArbre <= 0){
+                    Perso_arbre.style.display = "none";
+                }
+                break;
+            case 1:
+                pvPlante -= 10;
+                PV_Plante.innerHTML = pvPlante;
+                if(pvPlante <= 0){
+                    Perso_plante.style.display = "none";
+                }
+                break;
+            case 2:
+                pvSouche -= 10;
+                PV_Souche.innerHTML = pvSouche;
+                if(pvSouche <= 0){
+                    Perso_souche.style.display = "none";
+                }
+                break;
+            case 3:
+                pvRiviere -= 10;
+                PV_Riviere.innerHTML = pvRiviere;
+                if(pvRiviere <= 0){
+                    Perso_riviere.style.display = "none";
+                }
+                break;
+        }
+    }
+    tourDesPersos = true;
 }
 
 /*Fonctions dégats pour l'action attaque pour tous les monstres qui enlève 10PV*/
@@ -33,28 +82,60 @@ var pvTornadeHtml = document.getElementById("PVTornade");
 var pvTornade = "100";
 
 function degats_tornade(){
-    pvTornade -= 10;
-    pvTornadeHtml.innerHTML = pvTornade;
-
-    tonElement.style.visibility = "hidden"
-
-    tonElement.style.visibility = "visible"
+    if(persoVise){
+        pvTornade -= 10;
+        pvTornadeHtml.innerHTML = pvTornade;
+        persoVise = false;
+        tourPerso += 1;
+        if(tourPerso >= 4){
+            tourPerso = 0;
+            tourDesPersos = false;
+            ennemisAttaquent();
+        }
+    }
+    if(pvTornade <= 0){
+        Monstre_tornade.style.display = "none";
+    }
 }
 
 var pvCaillouHtml = document.getElementById("PVCaillou");
 var pvCaillou = "100";
 
 function degats_caillou(){
-    pvCaillou -= 10;
-    pvCaillouHtml.innerHTML = pvCaillou;
+    if(persoVise){
+        pvCaillou -= 10;
+        pvCaillouHtml.innerHTML = pvCaillou;
+        persoVise = false;
+        tourPerso += 1;
+        if(tourPerso >= 4){
+            tourPerso = 0;
+            tourDesPersos = false;
+            ennemisAttaquent();
+        }
+    }
+    if(pvCaillou <= 0){
+        Monstre_caillou.style.display = "none";
+    }
 }
 
 var pvFeuHtml = document.getElementById("PVFeu");
 var pvFeu = "100";
 
 function degats_feu(){
-    pvFeu -= 10;
-    pvFeuHtml.innerHTML = pvFeu;
+    if(persoVise){
+        pvFeu -= 10;
+        pvFeuHtml.innerHTML = pvFeu;
+        persoVise = false;
+        tourPerso += 1;
+        if(tourPerso >= 4){
+            tourPerso = 0;
+            tourDesPersos = false;
+            ennemisAttaquent();
+        }
+    }
+    if(pvFeu <= 0){
+        Monstre_feu.style.display = "none";
+    }
 }
 
 /*Fonction pour l'attaque spéciale vague du personnage rivière*/
@@ -66,27 +147,6 @@ function vague(){
 
 var pvTornadeHtml = document.getElementById("PVTornade");
 var pvTornade = "100";
-
-function degats_tornade_vague(){
-    pvTornade -= 20;
-    pvTornadeHtml.innerHTML = pvTornade;
-}
-
-var pvCaillouHtml = document.getElementById("PVCaillou");
-var pvCaillou = "100";
-
-function degats_caillou_vague(){
-    pvCaillou -= 20;
-    pvCaillouHtml.innerHTML = pvCaillou;
-}
-
-var pvFeuHtml = document.getElementById("PVFeu");
-var pvFeu = "100";
-
-function degats_feu_vague(){
-    pvFeu -= 20;
-    pvFeuHtml.innerHTML = pvFeu;
-}   
 
 /*Fonction soigner pour l'attaque spéciale du perso arbre
 function soigner(){
@@ -100,6 +160,18 @@ var pvArbreHtml = document.getElementById("PVArbre");
 var pvArbre = "100";
 
 function soigner(){
-    pvArbre -= (-20);
-    pvArbreHtml.innerHTML = pvArbre;
+    if(persoVise){
+        pvArbre -= (-20);
+        pvArbreHtml.innerHTML = pvArbre;
+        persoVise = false;
+        tourPerso += 1;
+        if(tourPerso >= 4){
+            tourPerso = 0;
+            tourDesPersos = false;
+            ennemisAttaquent();
+        }
+    }
+    if(pvArbre <= 0){
+        Perso_arbre.style.display = "none";
+    }
 }
