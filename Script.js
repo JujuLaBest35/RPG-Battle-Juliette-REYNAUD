5aed8f65e82420bdfdffa3ea6c207e00ff59a386
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2022-12-10 16:49:24 +0100

    Problèmes réglés

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
 /*Variables pour tous les personnages reliées à leurs identité dans HTML*/
 var Perso_arbre=document.getElementById("id_arbre")
-var Monstre_tornade=document.getElementById("id_tornade")
+var Monstre_1=document.getElementById("id_tornade")
 var Perso_plante=document.getElementById("id_plante")
-var Monstre_caillou=document.getElementById("id_caillou")
+var Monstre_2=document.getElementById("id_caillou")
 var Perso_souche=document.getElementById("id_souche")
-var Monstre_feu=document.getElementById("id_feu")
+var Monstre_3=document.getElementById("id_feu")
 var Perso_riviere=document.getElementById("id_riviere")
 var attaque_arbre=document.getElementById("attaque_arbre")
 
@@ -14,12 +14,19 @@ var PV_Plante=document.getElementById("PVPLante")
 var PV_Souche=document.getElementById("PVPlante")
 var PV_Riviere=document.getElementById["PvRiviere"]
 
+var tourPerso = 0;
+
+function RandomInt(min, max){
+    return (Math.floor(Math.random() * (max-min)) + min);
+}
+
 /*Fonction attaquer*/
 function attaquer(){
+    tourPerso = 0;
     Monstre_caillou.onclick = degats_caillou;
     Monstre_feu.onclick = degats_feu;
     Monstre_tornade.onclick = degats_tornade;
-    }
+}
 
 /*Fonctions dégats pour l'action attaque pour tous les monstres qui enlève 10PV*/
 var pvTornadeHtml = document.getElementById("PVTornade");
@@ -29,7 +36,6 @@ function degats_tornade(){
     pvTornade -= 10;
     pvTornadeHtml.innerHTML = pvTornade;
 }
-
 
 var pvCaillouHtml = document.getElementById("PVCaillou");
 var pvCaillou = "100";
@@ -54,8 +60,7 @@ function vague(){
     Monstre_caillou.onclick = degats_caillou_vague;
     Monstre_feu.onclick = degats_feu_vague;
     Monstre_tornade.onclick = degats_tornade_vague;
-    }
-
+}
 
 var pvTornadeHtml = document.getElementById("PVTornade");
 var pvTornade = "100";
@@ -81,13 +86,13 @@ function degats_feu_vague(){
     pvFeuHtml.innerHTML = pvFeu;
 }   
 
-/*Fonction soigner pour l'attaque spéciale du perso arbre*/
+/*Fonction soigner pour l'attaque spéciale du perso arbre
 function soigner(){
     Perso_arbre.onclick = ;
     Perso_plante.onclick = ;
     Perso_souche.onclick = ;
     Perso_riviere.onclick = 
-}
+}*/
 
 var pvArbreHtml = document.getElementById("PVArbre");
 var pvArbre = "100";
